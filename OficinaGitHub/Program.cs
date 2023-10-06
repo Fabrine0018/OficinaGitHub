96ef8666f51a4fd14e004736f64be8368c37e9e2
Fabrine <fabrine.velame@gmail.com>
2023-10-06 09:27:09 -0400

    Olá mundo! primiero commit

---
+++ OficinaGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" Digite outro número: ");
+        double num2= Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num * num2;
+        Console.WriteLine(" O resultado da multiplicação é " +  resultado);
+        
+        Console.ReadKey();
+    }
+}
